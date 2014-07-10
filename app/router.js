a30a13e9b2a2f6b4eac639e31a06aa15c96bc5ea
Dave-Choi <daaave@gmail.com>
2014-07-10 10:25:44 -0400

    Add main routes to router map

--- app/router.js
+++ app/router.js
@@ -4,7 +4,12 @@ var Router = Ember.Router.extend({
   location: PomelloENV.locationType
 });
 
-Router.map(function() {
+Router.map(function(){
+	this.resource("trello", function(){ // Transitioned to after Trello authorization
+		this.resource("cards", function(){
+			this.resource("card", { path: ":card_id" });
+		});
+	});
 });
 
 export default Router;
