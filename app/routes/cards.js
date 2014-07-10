e388f50469be642c5af825dfcc6a0f4afb528c90
Dave-Choi <daaave@gmail.com>
2014-07-10 10:35:51 -0400

    Add Cards templates and Route

---
+++ app/routes/cards.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import AuthorizedRoute from './authorized';
+
+export default AuthorizedRoute.extend({
+	model: function(){
+		var token = this.controllerFor("user").get("token");
+		return Ember.$.getJSON('https://trello.com/1/members/me/cards?key=69ea5c8500ae4ff25ecfdd6e4d92a561&token=' + token);
+	}
+});
