1771fd958d98046274c17b40dfaf323fa33e0e03
Dave-Choi <daaave@gmail.com>
2014-07-12 02:17:19 -0400

    Add basic TimerController and template

---
+++ app/controllers/timer.js
@@ -0,0 +1,70 @@
+/* global moment */
+
+import Ember from 'ember';
+
+function minutesToMilliseconds(minutes){
+	return minutes * 60 * 1000;
+}
+
+export default Ember.Controller.extend({
+	duration: minutesToMilliseconds(25),
+	startTime: null,
+	time: 0,
+	
+	isTiming: false,
+	timeoutID: null,
+
+	placeholder: function(){
+		return "My focus for the next " + moment.duration(this.get("duration")).humanize() + " is ...";
+	}.property("duration"),
+
+	remaining: function(){
+		return this.get("duration") - this.get("time");
+	}.property("time", "duration"),
+
+	remainingString: function(){
+		var remaining = this.get("remaining");
+		if(remaining <= 0){
+			return "finished";
+		}
+		return moment.duration(remaining).humanize();
+	}.property("remaining"),
+
+	completion: function(){
+		return this.get("time") / this.get("duration");
+	}.property("time", "duration"),
+
+	stepTime: function(){
+		var controller = this;
+		this.set("time", Date.now() - this.get("startTime"));
+
+		var time = this.get("time");
+		var duration = this.get("duration");
+
+		if(time >= duration){
+			// Do whatever happens when the timer's done.
+
+			this.send("stop");
+		}
+		else{
+			var timeoutID = setTimeout(function(){
+				controller.stepTime();
+			}, 1000);
+			this.set("timeoutID", timeoutID);
+		}
+		
+	},
+
+	actions: {
+		start: function(){
+			this.set("isTiming", true);
+			this.set("startTime", Date.now());
+			this.stepTime();
+		},
+
+		stop: function(){
+			this.set("isTiming", false);
+			clearTimeout(this.get("timeoutID"));
+		}
+	}
+});
