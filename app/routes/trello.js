5daf6536e4b331f9b4472889fee1a4985f62dc85
Dave-Choi <daaave@gmail.com>
2014-07-21 23:15:41 -0400

    Fix incorrect API URL in TrelloRoute
    Was getting cards, instead of the user.

--- app/routes/trello.js
+++ app/routes/trello.js
@@ -4,11 +4,12 @@ import AuthorizedRoute from './authorized';
 export default AuthorizedRoute.extend({
 	model: function(){
 		var token = this.controllerFor("user").get("token");
-		return Ember.$.getJSON('https://trello.com/1/members/me/cards?key=69ea5c8500ae4ff25ecfdd6e4d92a561&token=' + token);
+		return Ember.$.getJSON('https://trello.com/1/members/me?key=69ea5c8500ae4ff25ecfdd6e4d92a561&token=' + token);
 	},
 
 	setupController: function(controller, model){
 		var userController = this.controllerFor("user");
+		console.log(model);
 		userController.set("model", model);
 	}
 });
