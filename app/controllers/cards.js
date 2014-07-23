826856f8e51345e97dcfe9f9c077122522d40991
Dave-Choi <daaave@gmail.com>
2014-07-23 10:52:07 -0400

    Cards view improvements
    - Added CardsController, with filtering.
    - Filter input
    - Comment count badges
    - Smaller card titles
    - Card descriptions

---
+++ app/controllers/cards.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+	filterString: "",
+	filterExpression: function(){
+		return new RegExp(this.get("filterString"));
+	}.property("filterString"),
+
+	searchTargets: [
+		"name",
+		"desc"
+	],
+
+	filtered: function(){
+		return this.filter(function(card){
+			card = Ember.Object.create(card); // Make sure card has a get method
+
+			var filterExpression = this.get("filterExpression");
+			var targets = this.get("searchTargets");
+
+			var targetProperties = targets.map(function(targetName){
+				return card.get(targetName);
+			}).join(" "); // This just makes one string to test per card
+
+			return filterExpression.test(targetProperties);
+		}.bind(this));
+	}.property("model.@each", "filterExpression")
+});
