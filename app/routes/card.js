5a76f9986947f4f2cc2679c61388b5f0c2850fbc
Dave-Choi <daaave@gmail.com>
2014-07-25 07:47:42 -0400

    Refactor card route
    Was using a nested structure for things that could be fetched in
    parallel.

--- app/routes/card.js
+++ app/routes/card.js
@@ -3,13 +3,25 @@ import AuthorizedRoute from './authorized';
 
 export default AuthorizedRoute.extend({
 	model: function(params){
+		var appKey = "69ea5c8500ae4ff25ecfdd6e4d92a561";
 		var token = this.controllerFor("user").get("token");
 		var cardId = params.card_id;
-		return Ember.$.getJSON('https://trello.com/1/cards/' + cardId + '?key=69ea5c8500ae4ff25ecfdd6e4d92a561&token=' + token).then(function(card){
-			return Ember.$.getJSON('https://trello.com/1/cards/' + cardId + '/actions?filter=commentCard&key=69ea5c8500ae4ff25ecfdd6e4d92a561&token=' + token).then(function(comments){
+
+		var cardURL = 'https://trello.com/1/cards/' + cardId + '?key=' + appKey + '&token=' + token;
+		var commentURL = 'https://trello.com/1/cards/' + cardId + '/actions?filter=commentCard&key=' + appKey + '&token=' + token;
+
+		return Ember.RSVP.hash({
+			card: Ember.$.getJSON(cardURL),
+			comments: Ember.$.getJSON(commentURL)
+		}).then(
+			function(results){
+				var card = results.card;
+				var comments = results.comments;
+				
 				card.comments = comments;
+
 				return card;
-			});
-		});
+			}
+		);
 	}
 });
