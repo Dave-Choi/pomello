c861ef44939c7b34ae37489c19fddecfbed66460
Dave-Choi <daaave@gmail.com>
2014-08-19 16:58:59 -0400

    Cleanup

--- app/adapters/card.js
+++ app/adapters/card.js
@@ -3,19 +3,6 @@ import DS from 'ember-data';
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-	// find: function(store, type, id){
-	// 	var appKey = this.get("key");
-	// 	var token = this.get("token");
-
-	// 	var cardURL = 'https://trello.com/1/cards/' + id + '?key=' + appKey + '&token=' + token;
-	// 	var commentURL = 'https://trello.com/1/cards/' + id + '/actions?filter=commentCard&key=' + appKey + '&token=' + token;
-
-	// 	return Ember.RSVP.hash({
-	// 		card: Ember.$.getJSON(cardURL),
-	// 		comments: Ember.$.getJSON(commentURL)
-	// 	});
-	// },
-
 	findAll: function(store, type, sinceToken){
 	/*
 		Trello uses one URL for getting individual cards, but to
