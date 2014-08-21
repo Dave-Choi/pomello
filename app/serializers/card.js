068f13fcae95dcc45b53f2b0ac84742a0c963dd1
Dave-Choi <daaave@gmail.com>
2014-08-21 08:32:22 -0400

    Add link to Card on Trello in Card template

--- app/serializers/card.js
+++ app/serializers/card.js
@@ -3,6 +3,7 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend({
 	normalize: function(type, hash, prop){
 		hash.commentCount = hash.badges.comments;
+		hash.shortURL = hash.shortUrl;
 		return this._super(type, hash, prop);
 	}
 });
