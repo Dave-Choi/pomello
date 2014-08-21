ced63d05e26721248bc0554f2ef6c6d77b89572a
Dave-Choi <daaave@gmail.com>
2014-08-21 13:29:00 -0400

    Cleanup

--- app/routes/cards.js
+++ app/routes/cards.js
@@ -2,8 +2,6 @@ import AuthorizedRoute from './authorized';
 
 export default AuthorizedRoute.extend({
 	model: function(){
-		// var token = this.controllerFor("user").get("token");
-		// return Ember.$.getJSON('https://trello.com/1/members/me/cards?key=69ea5c8500ae4ff25ecfdd6e4d92a561&token=' + token);
 		return this.store.find("card");
 	}
 });
